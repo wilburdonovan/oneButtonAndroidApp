2e127a4e0de5b6e30968eb27fa3cb5d95e70fefb
Wilbur Donovan <wilbur.donovan91@gmail.com>
2016-07-13 23:32:26 +1000

    Push through full app

---
+++ app/src/main/java/com/wilburdonovan/onebutton/MainActivity.java
@@ -0,0 +1,75 @@
+package com.wilburdonovan.onebutton;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
+    private TextView myText;
+    private int timesClicked = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        myButton = (Button) findViewById(R.id.myButton);
+        myText = (TextView) findViewById(R.id.myText);
+
+        View.OnClickListener ourSpy = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timesClicked++;
+                myText.setText("Wilbur Rules x " + timesClicked);
+            }
+        };
+
+        myButton.setOnClickListener(ourSpy);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast toastMessage = Toast.makeText(this, myText.getText(), Toast.LENGTH_LONG);
+            toastMessage.show();
+            timesClicked = 0;
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
